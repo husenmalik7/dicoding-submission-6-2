c5f3503c9714bc5e64546fdec9973923aed68579
Husen <husenmalik7@gmail.com>
2025-09-15 06:59:57 +0700

    test: add threads reducer test

---
+++ src/states/threads/reducer.test.js
@@ -0,0 +1,63 @@
+/**
+ * test scenario for threadsReducer
+ *
+ * - threadReducers function
+ *  - should return the initial state when given by unknown action
+ *  - should return the threads when given by RECEIVE_THREADS action
+ *
+ */
+
+import { describe, it, expect } from "vitest";
+import threadsReducer from "./reducer";
+
+describe("threadReducers function", () => {
+	it("should return the initial state when given by unknown action", () => {
+		// arrange
+		const initialState = [];
+		const action = { type: "UNKNOWN" };
+		// action
+		const nextState = threadsReducer(initialState, action);
+		// assert
+		expect(nextState).toEqual(initialState);
+	});
+
+	it("should return the threads when given by RECEIVE_THREADS action", () => {
+		// arrange
+		const initialState = [];
+		const action = {
+			type: "RECEIVE_THREADS",
+			payload: {
+				threads: [
+					{
+						id: "thread-1",
+						title: "Thread Pertama",
+						body: "Ini adalah thread pertama",
+						category: "General",
+						createdAt: "2021-06-21T07:00:00.000Z",
+						ownerId: "users-1",
+						upVotesBy: [],
+						downVotesBy: [],
+						totalComments: 0,
+					},
+					{
+						id: "thread-2",
+						title: "Thread Kedua",
+						body: "Ini adalah thread kedua",
+						category: "General",
+						createdAt: "2021-06-21T07:00:00.000Z",
+						ownerId: "users-2",
+						upVotesBy: [],
+						downVotesBy: [],
+						totalComments: 0,
+					},
+				],
+			},
+		};
+
+		// action
+		const nextState = threadsReducer(initialState, action);
+
+		// assert
+		expect(nextState).toEqual(action.payload.threads);
+	});
+});
